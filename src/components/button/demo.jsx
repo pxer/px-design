d5cd1ac4b0aac65e3eba54120a6a160a0107e0e6
代璞 <daissmentii@gmail.com>
2017-02-23 16:51:57 +0800

    added buttons' type

--- src/components/button/demo.jsx
+++ src/components/button/demo.jsx
@@ -6,11 +6,36 @@ export default class Demo extends Component {
   render(){
     return (
       <div>
-        <Button type="primary" className="lg">Primary</Button>
-        <Button className="px-btn-default">Default</Button>
-        <Button >Default</Button>
+        <Button>Default</Button>
+        <Button type="primary">Primary</Button>
+        <Button type="ghost">Ghost</Button>
         <Button type="dashed">Dashed</Button>
+        <Button disabled>Disabled</Button>
+        <br />
+
+        <Button type="primary">Primary</Button>
         <Button type="danger">Danger</Button>
+        <Button type="warning">Warning</Button>
+        <Button type="success">Success</Button>
+        <br />
+
+        <Button>中文</Button>
+        <Button>English</Button>
+        <br />
+
+        <Button size="small">Small</Button>
+        <Button >Default</Button>
+        <Button size="large">Large</Button>
+        <br />
+
+        <Button size="small" shape="cicle">1</Button>
+        <Button shape="cicle">2</Button>
+        <Button size="large" shape="cicle">3</Button>
+        <br />
+
+        {/*
+          <Button ></Button>
+        */}
       </div>
     )
   }
--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -1,7 +1,25 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component, PropTypes, Children } from 'react';
 import classNames from 'classnames';
 import '../style/index.jsx'; // style 集合
 import './style/index.scss'; // button style
+
+const rxTwoCNChar = /^[\u4e00-\u9fa5]{2}$/;
+const isTwoCNChar = rxTwoCNChar.test.bind(rxTwoCNChar);
+function isString(str) {
+  return typeof str === 'string';
+}
+
+// Insert one space between two chinese characters automatically.
+function insertSpace(child) {
+  if (isString(child.type) && isTwoCNChar(child.props.children)) {
+    return React.cloneElement(child, {},
+                              child.props.children.split('').join(' '));
+  }
+  if (isString(child)) {
+    return <span>{child}</span>;
+  }
+  return child;
+}
 
 export default class Button extends Component {
 
@@ -10,24 +28,37 @@ export default class Button extends Component {
   }
 
   static propTypes = {
-    type: PropTypes.oneOf(['primary', 'ghost', 'dashed', 'danger']),
-    className: PropTypes.string
+    type: PropTypes.oneOf(['primary', 'ghost', 'dashed', 'danger', 'success', 'warning']),
+    className: PropTypes.string,
+    size: PropTypes.oneOf(['large', 'default', 'small']),
+    shape: PropTypes.oneOf(['circle', 'circle-outline'])
   }
 
   render(){
-    const { type, htmlType, className, prefixCls } = this.props;
+    const {
+      type, shape, size = '', htmlType, className, prefixCls, children, ...others
+    } = this.props;
+
+    const sizeCls = ({
+      large: 'lg',
+      small: 'sm'
+    })[size] || '';
 
     const classes = classNames(prefixCls, {
-      [`${prefixCls}-${type}`]: type
+      [`${prefixCls}-${type}`]: type,
+      [`${prefixCls}-${shape}`]: shape,
+      [`${prefixCls}-${sizeCls}`]: sizeCls
     }, className);
-    console.log(18, classes);
+
+    const kids = Children.map(children, insertSpace);
 
     return (
       <button
+        {...others}
         type={htmlType || 'button'}
         className={classes}
       >
-        primary
+        {kids}
       </button>
     );
   }
